1b8e25846214750baed162978c69c18746394b4a
Nicolás Alonso <nicolas706@live.cl>
2024-11-09 11:45:29 -0300

    Add files via upload

---
+++ mvc/vista/layouts/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MVC personas</title>
+    <link rel="stylesheet" href="vista/css/app.css">
+</head>
+<body>
+   <div class="panel">
+       <h1 class="text-center">E-COSECHA</h1>
+       
+       <!-- contenido-->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Aplicación MVC</title>
+    <link rel="stylesheet" href="vista/css/app.css"> <!-- Asegúrate de ajustar la ruta a tu archivo CSS -->
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Cosecha</a></li>
+                <li><a href="index.php?m=editarCosecha&id">Persona</a></li>
+                <!-- Agrega más enlaces según sea necesario -->
+            </ul>
+        </nav>
+    </header>
+    <div class="container">
